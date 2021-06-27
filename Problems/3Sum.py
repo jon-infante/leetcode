7b145b907dbc7b76fba100eb34fa95c9762d6193
inv0w <jonjoninfante@gmail.com>
2021-06-26 22:25:04 -0400

    3sum solution'

--- Problems/3Sum.py
+++ Problems/3Sum.py
@@ -1,50 +1,77 @@
 from typing import List
 
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         unfound = True
+#         solution_list = []
+#         set_list = []
+#         curr_i = 0
+#         curr_j = 1
+#         curr_k = 2
+
+#         if not nums:
+#             return []
+#         if nums == [0] or nums == [] or len(nums) < 3:
+#             return []
+
+
+#         while unfound:
+#             #If a triplets solution is found, save to list of solutions
+#             sequence = [nums[curr_i],nums[curr_j],nums[curr_k]]
+#             print(sequence)
+#             if sequence not in solution_list and set(sequence) not in set_list:
+#                 if sequence[0] + sequence[1] + sequence[2] == 0:
+#                     #Sort sequence
+#                     if sequence[0] > sequence[1]:
+#                         sequence[0], sequence[1] = sequence[1], sequence[0]
+#                     if sequence[1] > sequence[2]:
+#                         sequence[1], sequence[2] = sequence[2], sequence[1]
+#                         if sequence[0] > sequence[1]:
+#                             sequence[0], sequence[1] = sequence[1], sequence[0]
+#                     #Adds to list of solutions
+#                     solution_list.insert(0,sequence)
+#                     set_list.append(set(sequence))
+#             #If the last sequence has been reached
+#             if curr_i == (len(nums) - 3) and curr_j == (len(nums) - 2) and curr_k == (len(nums) - 1):
+#                 solution_list.sort()
+#                 return solution_list
+            
+#             if curr_k == len(nums) - 1:
+#                 if curr_j == len(nums) - 2:
+#                     curr_i += 1
+#                     curr_j = curr_i + 1
+#                     curr_k = curr_j + 1
+#                 curr_j += 1
+#                 curr_k = curr_j + 1
+#             else:
+#                 curr_k += 1
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        unfound = True
-        solution_list = []
-        set_list = []
-        curr_i = 0
-        curr_j = 1
-        curr_k = 2
-
-        if not nums:
+        if len(nums) < 3:
             return []
-        if nums == [0] or nums == [] or len(nums) < 3:
-            return []
-
-
-        while unfound:
-            #If a triplets solution is found, save to list of solutions
-            sequence = [nums[curr_i],nums[curr_j],nums[curr_k]]
-            if sequence not in solution_list and set(sequence) not in set_list:
-                if sequence[0] + sequence[1] + sequence[2] == 0:
-                    #Sort sequence
-                    if sequence[0] > sequence[1]:
-                        sequence[0], sequence[1] = sequence[1], sequence[0]
-                    if sequence[1] > sequence[2]:
-                        sequence[1], sequence[2] = sequence[2], sequence[1]
-                        if sequence[0] > sequence[1]:
-                            sequence[0], sequence[1] = sequence[1], sequence[0]
-                    #Adds to list of solutions
-                    solution_list.insert(0,sequence)
-                    set_list.append(set(sequence))
-            #If the last sequence has been reached
-            if curr_i == (len(nums) - 3) and curr_j == (len(nums) - 2) and curr_k == (len(nums) - 1):
-                solution_list.sort()
-                return solution_list
-            #Once k has reached its peak position, increment j and position k after j. repeating for i and j. Increment k otherwise
-            if curr_k == len(nums) - 1:
-                curr_j += 1
-                curr_k = curr_j + 1
-            else:
-                curr_k += 1
-            if curr_j == len(nums) - 2:
-                curr_i += 1
-                curr_j = curr_i + 1
-                curr_k = curr_j + 1
-
+        
+        answer = set()
+        nums.sort()
+        blank = []
+        for i in range(len(nums)):
+            first = nums[i]
+            left = i + 1
+            right = len(nums)-1
+            while left < right:
+                second = nums[left]
+                third = nums[right]
+                sums = first + second + third
+                if sums > 0:
+                    right -= 1
+                elif sums < 0:
+                    left += 1
+                else:
+                    answer.add(tuple([first, second, third]))
+                    left += 1
+                    right -= 1
+        
+        return answer
             
         
 
