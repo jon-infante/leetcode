bc7dbff3393498ef06a79bec90a8ccb66fc58834
inv0w <jonjoninfante@gmail.com>
2021-06-26 22:00:14 -0400

    3sum

--- Problems/3Sum.py
+++ Problems/3Sum.py
@@ -32,24 +32,28 @@ class Solution:
                     set_list.append(set(sequence))
             #If the last sequence has been reached
             if curr_i == (len(nums) - 3) and curr_j == (len(nums) - 2) and curr_k == (len(nums) - 1):
-                return solution_list   
+                solution_list.sort()
+                return solution_list
             #Once k has reached its peak position, increment j and position k after j. repeating for i and j. Increment k otherwise
             if curr_k == len(nums) - 1:
                 curr_j += 1
                 curr_k = curr_j + 1
-                if curr_j == len(nums) - 2:
-                    curr_i += 1
-                    curr_j = curr_i + 1
             else:
-                curr_k +=1
-           
+                curr_k += 1
+            if curr_j == len(nums) - 2:
+                curr_i += 1
+                curr_j = curr_i + 1
+                curr_k = curr_j + 1
 
+            
+        
 
 if __name__ == '__main__':
+    ok = [34,55,79,28,46,33,2,48,31,-3,84,71,52,-3,93,15,21,-43,57,-6,86,56,94,74,83,-14,28,-66,46,-49,62,-11,43,65,77,12,47,61,26,1,13,29,55,-82,76,26,15,-29,36,-29,10,-70,69,17,49]
     zeros = [0,0,0,0]
     nums0 = [-1,0,1,2,-1,-4,-2,-3,3,0,4]
     nums = [-1,0,1,2,-1,-4]
     nums2 = [-1,0,1]
     nums3 = [0]
     solution = Solution()
-    print(solution.threeSum(nums0))
+    print(solution.threeSum(ok))
