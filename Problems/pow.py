323ae3a298370afa82ff612031666c1b494ce7d6
inv0w <jonjoninfante@gmail.com>
2021-06-29 11:07:43 -0400

    test

---
+++ Problems/pow.py
@@ -0,0 +1,8 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
+        
+    
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.myPow(2.00000, -2))
