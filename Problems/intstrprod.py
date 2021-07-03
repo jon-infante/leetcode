2fc06e0a87741e7adbfade2c45a5e1940ffde07f
inv0w <jonjoninfante@gmail.com>
2021-07-02 21:41:47 -0400

    Non-implementation solution

--- Problems/intstrprod.py
+++ Problems/intstrprod.py
@@ -3,8 +3,8 @@
 
 # Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
 
-"""Basic solution"""
+"""Non-implementation solution"""
 # class Solution:
 #     def multiply(self, num1: str, num2: str) -> str:
 #         return str(int(num1)*int(num2))
-        
+
