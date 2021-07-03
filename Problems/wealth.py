8052adebf298ec92f7576617931e7760935986bf
inv0w <jonjoninfante@gmail.com>
2021-07-03 17:48:07 -0400

    wealth

---
+++ Problems/wealth.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        wealth_max = 0
+        for cust in accounts:
+            wealth = 0
+            for j in cust:
+                wealth += j
+            
+            if wealth > wealth_max:
+                wealth_max = wealth
+                
+        return wealth_max
